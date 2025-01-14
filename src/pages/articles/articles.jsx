28d2874c990088574b6ab24c5af459c86ddc32d5
fikayo <fikayoadele@gmail.com>
2025-01-14 17:45:44 +0100

    final

--- src/pages/articles/articles.jsx
+++ src/pages/articles/articles.jsx
@@ -2,24 +2,10 @@ import { Link } from "react-router-dom"
 import './articles.css'
 const Articles = () => {
 
-      const ismobile = () => window.innerWidth < 768
-
-      const today = new Date()
-
-      const formatDate = (date) => {
-        const currentDate = date;
-        const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-        const day = String(currentDate.getDate()).padStart(2, '0');
-        const year = String(currentDate.getFullYear()).slice(-2); 
-        
-        const formattedDate = `${month}/${day}/${year}`;
-        return formattedDate
-      }
-
     return (
         <section id="articles">
             <div id='articlesWrap'>
-                <h1 className="fs-500">Fikayo's engineering blog</h1>
+                <h1 className="fs-500">Engineering blog</h1>
                 
                     <Link to='/article/designPatterns'>
                     <div className='articles'>
